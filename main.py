26b2e2e7e5f8982c08a04afb947c1c041fc56cd0
Jean Lopin <jean.lopin@redlab.io>
2022-03-30 18:15:24 +0200

    Setup a logger

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import socket
+import logging
 
 if __name__ == "__main__":
-    print("Starting server...")
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)-8s [%(module)s.%(funcName)s():%(lineno)d] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.info("Starting server...")
 
     HOST = '0.0.0.0'          # Symbolic name meaning all available interfaces
     PORT = 50007              # Arbitrary non-privileged port
@@ -10,7 +16,7 @@ if __name__ == "__main__":
         s.listen(1)
         conn, addr = s.accept()
         with conn:
-            print('Connected by', addr)
+            logging.info(f"Connected by {addr}")
             while True:
                 data = conn.recv(1024)
                 if not data: break
